05b2c16cb44d139fbf1bedcd41c2e822d4fb38f0
Todaywyou <srirezekirahayu2424@gmail.com>
2025-12-28 11:25:59 +0700

    databases

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "PASTE_DARI_FIREBASE",
-  authDomain: "PASTE_DARI_FIREBASE",
-  projectId: "PASTE_DARI_FIREBASE",
-  storageBucket: "PASTE_DARI_FIREBASE",
-  messagingSenderId: "PASTE_DARI_FIREBASE",
-  appId: "PASTE_DARI_FIREBASE",
+  apiKey: "AIzaSyAzXhbs2lwdgzkEDlYpCa3lLnOeyGIug2M",
+  authDomain: "undangan-pernikahan-605f2.firebaseapp.com",
+  projectId: "undangan-pernikahan-605f2",
+  storageBucket: "undangan-pernikahan-605f2.firebasestorage.app",
+  messagingSenderId: "869714647248",
+  appId: "1:869714647248:web:625861c5e0cf83ed3466d5",
 };
 
 const app = initializeApp(firebaseConfig);
